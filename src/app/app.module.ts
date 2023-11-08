434319b20d3ce200d9b86bc03d1d11d90a0d4e06
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-08 14:23:41 -0500

    Added two way binding between the to-do component and its template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
@@ -12,7 +13,8 @@ import { ToDoComponent } from './container/to-do/to-do.component';
     ToDoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
