1f99720619705d692174f08acd6ec9a64bd40920
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-08 14:00:22 -0500

    Created container component and within that component created a child component called to-do. Using container component as the view template that will be rendered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { ToDoComponent } from './container/to-do/to-do.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    ToDoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/container/to-do/to-do.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'to-do',
+  templateUrl: './to-do.component.html',
+  styleUrls: ['./to-do.component.css']
+})
+export class ToDoComponent {
+
+}
